89bed3759e7a3c8fd1b6de15408c814177cdc11a
Pranabananda <sonujana33@gmail.com>
2023-03-25 10:40:00 +0530

    Added redux-persist library and required configuration

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import "./index.scss";
+import { PersistGate } from "redux-persist/integration/react";
 
 const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
